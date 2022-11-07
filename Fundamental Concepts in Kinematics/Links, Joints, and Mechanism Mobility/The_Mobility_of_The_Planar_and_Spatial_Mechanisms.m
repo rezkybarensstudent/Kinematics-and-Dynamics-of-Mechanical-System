b09d9b560a55559e669c0261132f62fbfa13844a
Rezky Putra Barens <92147766+rezkybarensstudent@users.noreply.github.com>
2022-11-07 15:28:25 +0700

    Add files via upload

---
+++ Fundamental Concepts in Kinematics/Links, Joints, and Mechanism Mobility/The_Mobility_of_The_Planar_and_Spatial_Mechanisms.m
@@ -0,0 +1,13 @@
+% The Mobility of The Planar and Spatial Mechanisms
+L = 8;
+J1 = 10;
+J2 = 0;
+DOF_Planar = 3*(L - 1) - 2*J1 - J2
+
+L = 6;
+J1 = 3;
+J2 = 1;
+J3 = 3;
+J4 = 0;
+J5 = 0;
+DOF_Spatial = 6*(L - 1) - 5*J1 - 4*J2 - 3*J3 - 2*J4 - J5
