ff41c812b4f9885fb6d1f5be22e7b9c786f1ab6a
Rezky Putra Barens <92147766+rezkybarensstudent@users.noreply.github.com>
2022-11-07 15:23:23 +0700

    Add files via upload

---
+++ Mathematical Concepts in Kinematics/Vector Point and Representation/Script.m
@@ -0,0 +1,12 @@
+% Vector and Point Representation
+% Example 1
+p1 = [1.25, -5]';
+p2 = [-2, 9.65]';
+V = p2 - p1
+Magnitude = norm(V)
+
+% Example 2
+p1 = [1.25, -5, 0]';
+p2 = [-2, 9.65, 6]';
+V = p2 - p1
+Magnitude = norm(V)
